e14ebb85812c88238ea64ad8b76cf2978f1f50cd
isadoraora <isa.mdeo@gmail.com>
2019-09-24 20:34:13 -0300

    ajustes

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,38 @@
 //resposta aqui
+//var nomeDaVariavel = "oliveira";
+//console.log(nomeDaVariavel)
+
+//Crie variaveis para o guardar o nome, idade, se é a primeira vez e uma lista produtos favoritos do cliente
+//let nome = prompt('Digite seu nome: ')
+//let idade = parseInt('Digite sua idade: ')
+//let primeiraVez = prompt('É sua primeira vez na loja? ')
+//let produtos = ["Calça", "Blusa", "Jaqueta", "Tênis"]
+
+//- Atribua valores a essas variaveis
+let nome, idade, primeiraVez;
+let produtos = ["Calça", "Blusa", "Jaqueta", "Tênis"]
+nome = 'Isadora';
+idade=30;
+primeiraVez = true;
+
+//Crie um objeto que irá receber os valores dessa variaveis e o imprima o objeto no console
+var meuObjeto = new Object()
+meuObjeto.nome = "Isadora";
+meuObjeto.idade = 30;
+meuObjeto.primeiraVez = true;
+meuObjeto.produtos = ["Calça", "Blusa", "Jaqueta", "Tênis"]
+console.log(meuObjeto)
+
+/*Resposta da Luma:
+var nome = 'Isa;
+var idade = 30;
+var primeiraVez = true;
+var favoritos = ['acai','hamburguer','pacoca']
+var luma{
+    nome:nome,
+    idade:idade,
+    primeiraVez:primeiraVez,
+    favoritos:favoritos
+};
+console.log(luma)
+*/
--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,30 @@
-//resposta aqui
+/*resposta aqui
+
+var r = require("readline-sync");
+var a = parseInt(r.question("Digite 1 ou 2 :"));
+console.log(a)
+- Crie uma variável que irá guardar o array com nome e valor dos produtos
+- Crie uma variável 'posicao' para guardar o produto escolhido pelo usuário
+- Crie uma variável 'porcentagem' que irá guardar qual porcentagem o usuário digitou
+- Procure no array qual produto foi escolhido pelo usuário
+- Calcule o desconto do produto
+- Printe na tela a seguinte frase: "Produto xxx com desconto é R$ xxx"
+
+Dica: você pode usar a função .toFixed(digitos) para limitar o numero de casas decimais do desconto.
+
+*/
+var r1 = require("readline-sync");
+
+var stuff = [
+{nome: 'Calça Jeans',valor: 40},
+{nome: 'Camiseta',valor: 20},
+{nome: 'Tênis',valor: 90},
+{nome: 'Bota',valor: 120}
+]
+var valor =0;
+var posicao = parseInt(r1.question('Digite a posição do produto, de 0 a 3: '))
+var porcentagem = parseInt(r1.question('Digite a porcentagem desejada: '))
+var escolhaCliente = stuff[posicao];
+var desconto = escolhaCliente.valor *(porcentagem/100);
+
+console.log(`Produto ${escolhaCliente} com desconto é R$ ${desconto.toFixed(2)}`);

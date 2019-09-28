1bd8ad85223aa303ac5d792adc3422bfb8d19a97
isadoraora <isa.mdeo@gmail.com>
2019-09-27 22:09:55 -0300

    exercicios

--- 6_desafios/exercicios.js
+++ 6_desafios/exercicios.js
@@ -42,6 +42,15 @@ console.log(`O maior número da lista é ${maior}`);
 let menor = Math.min.apply(null, operações);
 console.log(`O menor número da lista é ${menor}`);
 
-
-
-
+let array = [5, 37, 18, 59, 12, -5];
+let maior = 0;
+let menor = 0;
+for (let i = 0; i < array.length; i++) {
+    if (array[i] > maior) {
+        maior = array[i];
+    }
+    if (array[i] < menor) {
+        menor = array[i];
+    }
+}
+console.log
